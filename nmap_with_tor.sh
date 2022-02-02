1b9328cf3f4ca46b4924b86f12055ba5e1fa1882
Sagar Khandve <i.sagarkhandve@gmail.com>
2022-02-02 17:19:34 +0530

    Add files via upload

---
+++ nmap_with_tor.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+#
+# Author:       Sagar Khandve (i.sagarkhandve@gmail.com)
+#
+# Name:	        Nmap With Tor.
+#
+# Description:  A script that lets users run full TCP port scans with NMap
+#               anonymously through the TOR network. TOR and ProxyChains must be
+#               installed and configured, and the TOR daemon must be running.
+#
+################################################################################
+
+
+# FUNCTIONS --------------------------------------------------------------------
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1 || { echo "Command not found: $1" 1>&2 ; exit 1 ; }
+}
+
+valid_ip()
+{
+    local ip=$1
+    local stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] ; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+
+    return $stat
+}
+
+
+# CHECKS -----------------------------------------------------------------------
+
+declare -a CMDS=(
+"nmap"
+"proxychains"
+"tor"
+"tor-resolve"
+);
+
+for CMD in ${CMDS[@]} ; do
+    command_exists $CMD
+done
+
+
+# MAIN -------------------------------------------------------------------------
+
+TARGET=$1
+SOCKSPORT=9050
+OUT="/tmp/${TARGET}.out"
+
+if [[ ! -z ${TARGET} ]] ; then
+
+    if valid_ip ${TARGET} ; then
+        :
+    else
+        TARGET=$(tor-resolve ${TARGET} 127.0.0.1:${SOCKSPORT})
+    fi
+
+    #proxychains nmap -4 -sT -Pn -n -vv --open -oG ${OUT} ${TARGET}
+    proxychains nmap -4 -F -sT -Pn -n -v --open -oG ${OUT} ${TARGET}
+else
+    >&2 echo "Error! <TARGET> not specified."
+        echo "Usage: ./$(basename $BASH_SOURCE) <TARGET>"
+fi
